7e2eff94bf33faa5ea335a23425b4ba35d35f14d
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-06-21 00:09:24 -0500

    update

---
+++ firestore.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCauA0jI6KiBb5ge48jPYh0QaRQlQmMfi8",
+    authDomain: "summerfan-4905d.firebaseapp.com",
+    databaseURL: "https://summerfan-4905d-default-rtdb.firebaseio.com",
+    projectId: "summerfan-4905d",
+    storageBucket: "summerfan-4905d.appspot.com",
+    messagingSenderId: "177920797564",
+    appId: "1:177920797564:web:525af7a9136bb7c5c712f9",
+    measurementId: "G-YC35JKRE9B"
+  };
